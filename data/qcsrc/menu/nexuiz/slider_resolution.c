a38da2e8295fdf5d14a651659ecd6de83d799462
div0 <div0@f962a42d-fe04-0410-a3ab-8c8b0445ebaa>
2009-08-06 12:58:26 +0000

    r7327 | div0 | 2009-08-04 09:09:26 -0400 (Tue, 04 Aug 2009) | 2 lines CTS and Nexball in mapinfo dialog r7328 | div0 | 2009-08-04 09:38:49 -0400 (Tue, 04 Aug 2009) | 2 lines turn off anisotropy at medium and normal quality levels, as it causes lots of moiree on geforce 7 series r7329 | div0 | 2009-08-04 09:57:55 -0400 (Tue, 04 Aug 2009) | 2 lines experimental change: why was R_AddEntities there? we'll find out r7330 | div0 | 2009-08-04 10:09:52 -0400 (Tue, 04 Aug 2009) | 2 lines tiny projectile trail fix r7331 | mrbougo | 2009-08-04 11:45:30 -0400 (Tue, 04 Aug 2009) | 1 line fix nexball team counts r7332 | m0rfar | 2009-08-04 14:10:43 -0400 (Tue, 04 Aug 2009) | 1 line cp01.mapinfo typos r7333 | m0rfar | 2009-08-04 14:44:01 -0400 (Tue, 04 Aug 2009) | 1 line brighter team hud r7334 | div0 | 2009-08-04 14:52:22 -0400 (Tue, 04 Aug 2009) | 2 lines fix bot name selection in campaign r7335 | div0 | 2009-08-04 15:13:48 -0400 (Tue, 04 Aug 2009) | 2 lines fix "white team bug" r7336 | div0 | 2009-08-04 15:14:57 -0400 (Tue, 04 Aug 2009) | 2 lines slightly cleanup of last fix r7337 | div0 | 2009-08-04 15:24:58 -0400 (Tue, 04 Aug 2009) | 2 lines fix last fix r7338 | div0 | 2009-08-04 15:43:53 -0400 (Tue, 04 Aug 2009) | 2 lines sv_doublejump: match engine r7339 | div0 | 2009-08-05 02:03:50 -0400 (Wed, 05 Aug 2009) | 2 lines effect config overview script r7340 | div0 | 2009-08-05 02:07:45 -0400 (Wed, 05 Aug 2009) | 2 lines hide "ultimate" config from menu r7341 | tzork | 2009-08-05 04:43:21 -0400 (Wed, 05 Aug 2009) | 1 line FL_NOTARGET for buttons and no expensive classname check for turrets. r7342 | div0 | 2009-08-05 05:54:04 -0400 (Wed, 05 Aug 2009) | 2 lines drag box editor in sv_cheats; good for quick and dirty race-checkpointing of mikee maps r7343 | div0 | 2009-08-05 06:07:10 -0400 (Wed, 05 Aug 2009) | 2 lines fix output to annoy mappers/radiant less r7344 | div0 | 2009-08-05 06:34:43 -0400 (Wed, 05 Aug 2009) | 2 lines properly write the race ents again r7345 | div0 | 2009-08-05 07:30:05 -0400 (Wed, 05 Aug 2009) | 2 lines make most aspect ratios 600-based r7346 | div0 | 2009-08-05 07:40:03 -0400 (Wed, 05 Aug 2009) | 2 lines latch vid_con* cvars r7347 | div0 | 2009-08-05 09:29:56 -0400 (Wed, 05 Aug 2009) | 2 lines show only available resolutions r7348 | div0 | 2009-08-05 14:50:13 -0400 (Wed, 05 Aug 2009) | 2 lines enable vid_resizable by default, I think nobody will complain :P r7349 | div0 | 2009-08-06 03:23:27 -0400 (Thu, 06 Aug 2009) | 2 lines mediasource for tutorial; map compiler script update r7350 | div0 | 2009-08-06 03:26:49 -0400 (Thu, 06 Aug 2009) | 4 lines the tutorial... commit with mediasource follows later r7351 | div0 | 2009-08-06 08:22:49 -0400 (Thu, 06 Aug 2009) | 2 lines tooltip update r7352 | div0 | 2009-08-06 08:35:31 -0400 (Thu, 06 Aug 2009) | 2 lines lots of svn:eol-style native r7353 | div0 | 2009-08-06 08:36:00 -0400 (Thu, 06 Aug 2009) | 2 lines typo fix r7354 | div0 | 2009-08-06 08:50:02 -0400 (Thu, 06 Aug 2009) | 2 lines makebuild.sh: reenable PPC macs for now; minor fixes
    git-svn-id: svn://svn.icculus.org/nexuiz/branches/nexuiz-2.0@7356 f962a42d-fe04-0410-a3ab-8c8b0445ebaa

--- data/qcsrc/menu/nexuiz/slider_resolution.c
+++ data/qcsrc/menu/nexuiz/slider_resolution.c
@@ -18,33 +18,52 @@ entity makeNexuizResolutionSlider()
 }
 void addResolutionNexuizResolutionSlider(entity me, float w, float h, float cw, float ch)
 {
+	float f;
+	f = max(cw / w, ch / h);
+	if(f > 1)
+	{
+		cw = rint(cw / f);
+		ch = rint(ch / f);
+	}
 	me.addValue(me, strzone(strcat(ftos(w), "x", ftos(h))), strzone(strcat(ftos(w), " ", ftos(h), " ", ftos(cw), " ", ftos(ch))));
 	// FIXME (in case you ever want to dynamically instantiate this): THIS IS NEVER FREED
 }
 void configureNexuizResolutionSliderNexuizResolutionSlider(entity me)
 {
+	float i;
+	vector r0, r, c;
+
 	me.configureNexuizTextSlider(me, "vid_width");
-	me.addResolution(me, 640, 480, 640, 480);
-	me.addResolution(me, 800, 480, 800, 480);
-	me.addResolution(me, 800, 600, 800, 600);
-	me.addResolution(me, 1024, 600, 800, 468.75);
-	me.addResolution(me, 1024, 768, 800, 600);
-	me.addResolution(me, 1152, 864, 800, 600);
-	me.addResolution(me, 1280, 800, 800, 500);
-	me.addResolution(me, 1280, 960, 800, 600);
-	me.addResolution(me, 1280, 1024, 800, 640);
-	me.addResolution(me, 1440, 900, 800, 500);
-	me.addResolution(me, 1600, 900, 800, 450);
-	me.addResolution(me, 1600, 1200, 800, 600);
-	me.addResolution(me, 1680, 1050, 800, 500);
-	me.addResolution(me, 1920, 1080, 800, 450);
-	me.addResolution(me, 1920, 1200, 800, 500);
-	me.addResolution(me, 2048, 1536, 800, 600);
+
+	c = '0 0 0';
+	r0 = '0 0 0';
+	for(i = 0;; ++i)
+	{
+		r = getresolution(i);
+		if(r_x == 0 && r_y == 0)
+			break;
+		if(r == r0)
+			continue;
+		r0 = r;
+		if(r_x < 640 || r_y < 400)
+			continue;
+		c_x = 800;
+		c_y = rint(c_x * r_y / r_x);
+		if(c_y >= 600)
+		{
+			me.addResolution(me, r_x, r_y, c_x, c_y);
+			continue;
+		}
+		c_y = 600;
+		c_x = rint(c_y * r_x / r_y);
+		me.addResolution(me, r_x, r_y, c_x, c_y);
+	}
+
 	me.configureNexuizTextSliderValues(me);
 }
 void loadCvarsNexuizResolutionSlider(entity me)
 {
-	me.setValueFromIdentifier(me, strcat(cvar_string("vid_width"), " ", cvar_string("vid_height"), " ", cvar_string("vid_conwidth"), " ", cvar_string("vid_conheight")));
+	me.setValueFromIdentifier(me, strcat(cvar_string("vid_width"), " ", cvar_string("vid_height"), " ", cvar_string("menu_vid_conwidth"), " ", cvar_string("menu_vid_conheight")));
 }
 void saveCvarsNexuizResolutionSlider(entity me)
 {
@@ -53,8 +72,8 @@ void saveCvarsNexuizResolutionSlider(entity me)
 		tokenize_console(me.getIdentifier(me));
 		cvar_set("vid_width", argv(0));
 		cvar_set("vid_height", argv(1));
-		cvar_set("vid_conwidth", argv(2));
-		cvar_set("vid_conheight", argv(3));
+		cvar_set("menu_vid_conwidth", argv(2));
+		cvar_set("menu_vid_conheight", argv(3));
 	}
 }
 #endif
